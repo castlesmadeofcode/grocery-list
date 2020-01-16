f89d97a146f38fa77f9a22a5e9709f3bf27ed336
castle <castlesmadeofcode@gmail.com>
2020-01-16 11:03:06 -0600

    practiced manipulating the dom with javascript using querySelector and quertSelectorAll

---
+++ main.js
@@ -0,0 +1,25 @@
+console.log("Hello, World!");
+
+const four = 2 + 2;
+console.log(four);
+
+
+// find element with id of groceries
+// (query selector always targets the first element in an html file with a matching class)
+const groceriesArticle = document.querySelector("#groceries");
+// add class of red-text to element with id of groceries
+groceriesArticle.classList.add("red-text");
+
+//find element with class of groceriesHeader
+const groceriesHeader = document.querySelector(".groceriesHeader");
+//remove groceriesHeader class
+groceriesHeader.classList.remove("groceriesHeader");
+
+//grabs all list items 
+const groceryList = document.querySelectorAll("#groceryList li");
+//loops through list and adds groceryHeader class to each list item
+for (let i=0; i<groceryList.length; i++) {
+    if (i === 1) {
+    groceryList[i].classList.add("groceriesHeader");
+    }    
+}
